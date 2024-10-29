a89ec02fce0da144fa6d440e516997003ef34610
uniboxx <unibox@duck.com>
2024-10-29 17:34:37 +0100

    edit jobs

--- src/lib/components/ExperienceTable.svelte
+++ src/lib/components/ExperienceTable.svelte
@@ -7,27 +7,27 @@
   }
   const workExperience: WorkExperience[] = [
     {
-      jobTitle: "Software Engineer",
-      company: "cleverklagen",
-      startDate: "2021-03",
+      jobTitle: "Learning Web Development",
+      company: "none",
+      startDate: "2020",
     },
     {
-      jobTitle: "Data Scientist",
-      company: "FootballRadar",
-      startDate: "2019-08",
-      endDate: "2020-11",
+      jobTitle: "Telecomunications seller",
+      company: "Tim",
+      startDate: "2014-09",
+      endDate: "2019-11",
     },
     {
-      jobTitle: "Data Scientist",
-      company: "DAZN",
-      startDate: "2018-11",
-      endDate: "2019-07",
+      jobTitle: "Renovable Energy devices seller",
+      company: "Enel Green Power",
+      startDate: "2011-09",
+      endDate: "2014-07",
     },
     {
-      jobTitle: "Data Scientist - Master Thesis",
-      company: "FC Barcelona",
-      startDate: "2018-02",
-      endDate: "2018-08",
+      jobTitle: "Financial consultant",
+      company: "Banca Fideuram",
+      startDate: "2008-02",
+      endDate: "2011-08",
     },
   ];
 </script>
